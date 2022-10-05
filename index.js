c93b1f6b575c7e79f11ffaa5b8e36a09adc54a03
Steve Bennett <me@stevebennett.me>
2022-10-05 16:50:21 +1100

    Quiet option

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-const {promisify} = require("es6-promisify");
+const { promisify } = require('es6-promisify');
 const mercator = require('global-mercator');
 const vt2geojson = promisify(require('@mapbox/vt2geojson'));
-let pointInPolygon = require('@turf/boolean-point-in-polygon')
+let pointInPolygon = require('@turf/boolean-point-in-polygon');
 
-async function queryLngLat(tilesUrl, lngLat, zoom, layer, options = {}) {
+async function queryLngLat(
+    tilesUrl,
+    lngLat,
+    zoom,
+    layer,
+    options = { quiet: false }
+) {
     const [x, y, z] = mercator.lngLatToGoogle(lngLat, zoom);
-    const uri = tilesUrl
-        .replace('{x}', x)
-        .replace('{y}', y)
-        .replace('{z}', z);
+    const uri = tilesUrl.replace('{x}', x).replace('{y}', y).replace('{z}', z);
     try {
         const featureCollection = await vt2geojson({ uri, layer });
-        const features = featureCollection.features.filter(f => pointInPolygon(lngLat, f));
+        const features = featureCollection.features.filter((f) =>
+            pointInPolygon(lngLat, f)
+        );
         if (features) {
-            return { 
+            return {
                 type: 'FeatureCollection',
-                features
-            }
+                features,
+            };
         }
     } catch (e) {
-        console.error(e);
+        if (!quiet) {
+            console.error(e);
+        }
     }
 }
 module.exports = {
     queryLngLat,
-}
+};
